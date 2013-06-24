ef1e3bc7e827333f189fd480979a44e3cf710014
maryangeles <mary.corazoncito18@gmail.com>
2013-06-24 13:44:38 -0400

    Update App.java

--- Documents/NetBeansProjects/pruebas_de_integracion/src/main/java/com/mycompany/pruebas_de_integracion/App.java
+++ Documents/NetBeansProjects/pruebas_de_integracion/src/main/java/com/mycompany/pruebas_de_integracion/App.java
@@ -161,6 +161,25 @@ break;
 }
 
 }
+ int a=0, e=0;
+    	 System.out.println(".Bienvenido al Gato.");
+		 System.out.println("''''''''''''''''''''");
+		 Scanner sc = new Scanner(System.in);
+		 Gato gato = new Gato();
+             System.out.println();
+			 System.out.println("Presione el Numero de la Posicion");
+		while(e < 9){
+			 System.out.println("-------------");
+			 System.out.print("Posicion -> ");
+			 a = sc.nextInt();
+			 gato.Posicion(a);
+			 e++;
+			 if(e==9){
+				 System.out.println();
+					System.out.println("Ninguno Gano!!");
+				 break;
+			 }
+		 }
 
     }
 }
