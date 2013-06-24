9a3795f09aee7de0f68a8681dea581cd1e48cc67
maryangeles <mary.corazoncito18@gmail.com>
2013-06-24 12:58:37 -0500

    Update prueba.java

--- Documents/NetBeansProjects/pruebas_de_integracion/src/main/java/prueba.java
+++ Documents/NetBeansProjects/pruebas_de_integracion/src/main/java/prueba.java
@@ -8,5 +8,24 @@
  * @author LIZZY-isis
  */
 public class prueba {
-    
+    public static void main(String[] args) {
+        int a=0, e=0;
+    	 System.out.println(".Bienvenido al Gato.");
+		 System.out.println("''''''''''''''''''''");
+		 Scanner sc = new Scanner(System.in);
+		 Gato gato = new Gato();
+             System.out.println();
+			 System.out.println("Presione el Numero de la Posicion");
+		while(e < 9){
+			 System.out.println("-------------");
+			 System.out.print("Posicion -> ");
+			 a = sc.nextInt();
+			 gato.Posicion(a);
+			 e++;
+			 if(e==9){
+				 System.out.println();
+					System.out.println("Ninguno Gano!!");
+				 break;
+			 }
+		 }
 }
